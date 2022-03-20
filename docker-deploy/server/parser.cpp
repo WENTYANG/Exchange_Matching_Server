db06c3a27eb0bf6aa41b990de069a866c574c806
WENTYANG <ladadee1031@outlook.com>
2022-03-20 16:56:57 -0400

    complete XML parser and test

--- docker-deploy/server/parser.cpp
+++ docker-deploy/server/parser.cpp
@@ -1,23 +1,78 @@
 #include "parser.h"
-using namespace tinyxml2;
 
-XMLDocument* convert_to_file(string xml){
-    XMLDocument* doc = new XMLDocument;
-    doc.Parse(xml);
+// Remember to delete xml doc after parsing/writing into the database
+XMLDocument* convert_to_file(string xml) {
+    tinyxml2::XMLDocument* doc = new XMLDocument();
+    doc->Parse(xml.c_str());
     return doc;
 }
 
-int request_type(XMLDocument* xml){
-    XMLElement* root = doc.RootElement();
-    if(strcmp(root->value(), "create")==0) return CREATE;
-    else if(strcmp(root->value(), "transactions")==0) return TRANSACTION;
-    else{
+int request_type(XMLDocument* xml) {
+    XMLElement* root = xml->RootElement();
+    if (strcmp(root->Name(), "create") == 0)
+        return CREATE;
+    else if (strcmp(root->Name(), "transactions") == 0)
+        return TRANSACTION;
+    else {
         throw(MyException("Invalid request type"));
     }
 }
 
-CreateRequest parse_create(XMLDocument* xml){
+// Remember to delete request after writing into the database
+CreateRequest* parse_create(XMLDocument* xml) {
+    CreateRequest* request = new CreateRequest();
+    XMLElement* root = xml->RootElement();
+    XMLElement* currElem = root->FirstChildElement();
+    while (currElem) {
+        // const XMLAttribute *attribute=currElem->FirstAttribute();
+        // const XMLAttribute* id_attr = currElem->FindAttribute("account_id")
 
+        if (strcmp(currElem->Name(), "account") == 0) {  // Create accounts
+            int id = currElem->FirstAttribute()->IntValue();
+            // currElem->FirstAttribute()->Next()
+            int balance = currElem->FindAttribute("balance")->IntValue();
+            Account newAccount(id, balance);
+            request->accounts.push_back(newAccount);
+        } else if (strcmp(currElem->Name(), "symbol") == 0) {  // Create symbols
+            string sym = currElem->FirstAttribute()->Value();
+            Symbol symbol(sym);
+            XMLElement* currAcount = currElem->FirstChildElement();
+            while (currAcount) {
+                int id = currAcount->FirstAttribute()->IntValue();
+                int num = atoi(currAcount->GetText());
+                symbol.shares.push_back(Share(id, num));
+                currAcount = currAcount->NextSiblingElement();
+            }
+            request->symbols.push_back(symbol);
+        }
+
+        currElem = currElem->NextSiblingElement();
+    }
+    return request;
 }
 
-TransRequest parse_trans(XMLDocument* xml){}
+TransRequest* parse_trans(XMLDocument* xml) {
+    TransRequest* request = new TransRequest();
+    XMLElement* root = xml->RootElement();
+    request->account_id = root->FirstAttribute()->IntValue();
+    XMLElement* currElem = root->FirstChildElement();
+    while (currElem) {
+        if (strcmp(currElem->Name(), "order") == 0) {
+            string sym = currElem->FirstAttribute()->Value();
+            int amount = currElem->FindAttribute("amount")->IntValue();
+            int limit = currElem->FindAttribute("limit")->IntValue();
+            Order order(sym, amount, limit);
+            request->orders.push_back(order);
+        }
+        if (strcmp(currElem->Name(), "query") == 0) {
+            int trans_id = currElem->FirstAttribute()->IntValue();
+            request->queries.push_back(trans_id);
+        }
+        if (strcmp(currElem->Name(), "cancel") == 0) {
+            int trans_id = currElem->FirstAttribute()->IntValue();
+            request->cancels.push_back(trans_id);
+        }
+        currElem = currElem->NextSiblingElement();
+    }
+    return request;
+}
--- docker-deploy/server/parser.h
+++ docker-deploy/server/parser.h
@@ -1,10 +1,10 @@
-#include "tinyxml2.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <cstring>
+#include <iostream>
 #include <vector>
-#include <stdio.h>
-#include <exception>
+#include "exception.h"
+#include "tinyxml2.h"
 
 #define CREATE 1
 #define TRANSACTION 0
@@ -13,66 +13,82 @@ using namespace tinyxml2;
 using namespace std;
 
 /* ------------------------ Create request ------------------------ */
-class Account{
-public:
+class Account {
+   public:
     int account_id;
     int balance;
-}
-class Symbol{
-    /* A string of symbol name and a vector of account_ids 
+    Account(int id, int balance) : account_id(id), balance(balance){};
+};
+class Share {
+   public:
+    int account_id;
+    int num;
+    Share(int id, int num) : account_id(id), num(num){};
+};
+class Symbol {
+    /* A string of symbol name and a vector of account_ids
     and the number of shares in that account */
-public:
+   public:
     string sym;
-    typedef struct share_t{
-        int account_id;
-        int num;
-    } share;
-    vector<share> shares;
-}
-class CreateRequest{
-public:
+    vector<Share> shares;
+    Symbol(string sym) : sym(sym){};
+};
+class CreateRequest {
+   public:
     vector<Account> accounts;
     vector<Symbol> symbols;
-}
+    void printRequest() {
+        cout << "CreateRequest" << endl;
+        cout << "Accounts" << endl;
+        for (auto a : accounts) {
+            cout << "id: " << a.account_id << " balance:" << a.balance << endl;
+        }
+        cout << "Symbols" << endl;
+        for (auto s : symbols) {
+            cout << "sym: " << s.sym << endl;
+            for (auto sh : s.shares) {
+                cout << "id: " << sh.account_id << " num:" << sh.num << endl;
+            }
+        }
+    }
+};
 
 /* ------------------------ Transaction request ------------------------ */
-class Transaction{
-public:
-    int account_id;
-}
-class Order:public Transaction{
-public:
+class Order {
+   public:
     string sym;
     int amount;
-    int limit_price;
-}
-class Query:public Transaction{
-public:
-    int trans_id;
-}
-class Cancel:public Transaction{
-public:
-    int trans_id;    
-}
-class TransRequest{
+    int limit;
+    Order(string sym, int amount, int limit)
+        : sym(sym), amount(amount), limit(limit){};
+};
+
+class TransRequest {
+   public:
+    int account_id;
     vector<Order> orders;
-    vector<Query> queries;
-    vector<Cancel> cancels;
-}
+    vector<int> queries;
+    vector<int> cancels;
+    void printRequest() {
+        cout << "TransRequest" << endl;
+        cout << "Orders" << endl;
+        for (auto o : orders) {
+            cout << "sym: " << o.sym << " amount:" << o.amount
+                 << " limit:" << o.limit << endl;
+        }
+        cout << "Queries" << endl;
+        for (auto q : queries) {
+            cout << q << endl;
+        }
+        cout << "Cancels" << endl;
+        for (auto c : cancels) {
+            cout << c << endl;
+        }
+    }
+};
 
 /* ------------------------ Parse functions ------------------------ */
 XMLDocument* convert_to_file(string xml);
 int request_type(XMLDocument* xml);
-CreateRequest parse_create(XMLDocument* xml);
-TransRequest parse_trans(XMLDocument* xml);
-
-/* ------------------------ exception helper ------------------------ */
-class MyException : public std::exception {
-   private:
-    std::string msg;
-
-   public:
-    MyException(std::string msg);
-
-    const char* what() const throw();
-};
+CreateRequest* parse_create(XMLDocument* xml);
+TransRequest* parse_trans(XMLDocument* xml);
