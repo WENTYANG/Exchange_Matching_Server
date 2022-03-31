257292ec7f7bde540cbc00088a1ff45d6bb0796c
WENTYANG <ladadee1031@outlook.com>
2022-03-31 16:46:51 -0400

    client request/response directory

--- docker-deploy/testing/Client.h
+++ docker-deploy/testing/Client.h
@@ -13,7 +13,7 @@
 #include "tinyxml2.h"
 
 #define MAX_LENGTH 65536
-#define N_Thread_CREATE 20  //用于创建并发送create type的线程数量
+#define N_Thread_CREATE 2  //用于创建并发送create type的线程数量
 #define N_Thread_TRANS 3  //用于创建并发送transaction type的线程数量
 #define NUM_SYMBOL 2      //每个Create Request中添加的symbol数量
 #define INITIAL_SYMBOL_AMOUNT 100  //每个symbol的初始amount
